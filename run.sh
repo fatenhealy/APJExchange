d6e492ed244fec5c22f85e59716cd25aec5a3ec8
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-10-20 13:12:11 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 gitresult=$( { git status | grep "fatal: Not a git repository" > /dev/null; } 2>&1 )
-if [[ ! -d "./sample-Tasks" && "${gitresult}" != "" ]]; then
+if [[ ! -d "./sample-TelerikNEXT" && "${gitresult}" != "" ]]; then
 	# Cloning to default dir
     echo "=> Downloading sample-TelerikNEXT from git"
     printf "\r=> "
     command git clone "git@github.com:NativeScript/sample-TelerikNEXT.git"
-    cd sample-Tasks
+    cd sample-TelerikNEXT
 fi
 
 set -e
 npmresult=$(npm list -g | grep 'nativescript@');
 if [[ "$npmresult" == "" ]]; then
     # Install nativescript
-    npm install -g nativescript@1.3.0
+    npm install -g nativescript@1.4.2
 fi
 
 # Cleanup
@@ -24,13 +24,13 @@ rm -rf node_modules 2>/dev/null
 tns install
 
 # App needs some permissions and currently we copy custom AndroidManifest.xml.
-# This will be solved with {N} 1.4+ 
+# This will be solved with {N} 1.5+ 
 cp -R install/android platforms/
 cp -R install/ios platforms/
 
 # Get TypeScript definitions and extract to tns-core-modules
-curl -L https://github.com/NativeScript/NativeScript/releases/download/v1.3.0/tns-definitions-1.3.0.tgz > tns-definitions-1.3.0.tgz
-tar -xzkf tns-definitions-1.3.0.tgz --strip 1 -C node_modules/tns-core-modules 2>/dev/null
+curl -L https://github.com/NativeScript/NativeScript/releases/download/v1.4.0/tns-definitions-1.4.0.tgz > tns-definitions-1.4.0.tgz
+tar -xzkf tns-definitions-1.4.0.tgz --strip 1 -C node_modules/tns-core-modules 2>/dev/null
 
 # Build TypeScript
 grunt
